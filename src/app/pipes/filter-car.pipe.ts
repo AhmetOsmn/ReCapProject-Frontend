c6e3789c31089fe1d966ea6319b7689dc969429e
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-03-22 23:40:24 +0300

    19. Gun Odev 3 Commit 5

--- src/app/pipes/filter-car.pipe.ts
+++ src/app/pipes/filter-car.pipe.ts
@@ -7,8 +7,14 @@ import { Car } from '../models/car/car';
 export class FilterCarPipe implements PipeTransform {
 
   transform(value: Car[], filterText: string): Car[] {
-    filterText = filterText?filterText.toLocaleLowerCase():"";
-    return filterText?value.filter((c:Car)=>c.carName.toLocaleLowerCase().indexOf(filterText)!==-1):value;
+    return filterText?value
+    .filter((c:any) => this.matchValue(c,filterText))
+    :value;
+  }
+  matchValue(c:any, filterText:string) {
+    return Object.keys(c).map((key) => {
+       return new RegExp(filterText, 'gi').test(c[key]);
+    }).some(result => result);
   }
 
 }
